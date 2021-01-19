29c9b5051f406f6053b7ba30b72c2f9e4432dc56
VamerTyv <602710519@qq.com>
2021-01-19 16:14:50 +0800

    多环境配置2

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ROOT: '"http://127.0.0.1/api"'
+  API_ROOT: '"http://127.0.0.1:8843/api"',
+  HOST: '"http://127.0.0.1"',
+  PORT: "8081"
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_ROOT: '"http://192.168.136.129:8843"'
+  API_ROOT: '"http://192.168.136.129:8843"',
+  HOST: '"http://192.168.136.129"',
+  PORT: "8081"
 }
